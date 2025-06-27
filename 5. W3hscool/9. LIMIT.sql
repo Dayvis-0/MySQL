dcc4f53bae2624fdbb48760ab883e37bbb776c74
dayvis <amdayvis39@gmail.com>
2025-06-26 22:08:32 -0500

    26/06/2025

--- 5. W3hscool/9. LIMIT.sql
+++ 5. W3hscool/9. LIMIT.sql
@@ -7,5 +7,25 @@ WHERE condition
 LIMIT number;*/
 
 SELECT * FROM Customers;
-INSERT INTO Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country);
+INSERT INTO Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country)
 VALUES (1, 'Alfreds Futterkiste', 'Maria Anders', 'Obere Str. 57', 'Berlin', 12209, 'Germany');
+
+SELECT * FROM Customers 
+LIMIT 3;
+/*Como se selecciona los 4-6 registros?
+MySQL proporciona una forma de manejar esto: mediante OFFSET
+OFFSET -> Se usa junto con LIMIT para saltar un numero especifico de registros antes de comenzar a devolver resultado
+En simples palabras -> OFFSET le dice a MySQL cuantos registros debe saltarse desde el principio 
+antes de comenzar a dovolver resultdos*/
+SELECT * FROM Customers
+LIMIT 3 OFFSET 3;
+
+-- Con WHERE
+SELECT * FROM Customers
+WHERE Country = 'Germany'
+LIMIT 3;
+
+-- Con ORDER BY
+SELECT * FROM Customers
+ORDER BY Country 
+LIMIT 3;
